4f8ef1ce6b9038ef9dc68c3d970f37af5673fd26
Jenderson Quintero <jendersonq.20@gmail.com>
2022-11-13 23:58:02 -0400

    Organización y estructuración del proyecto

--- src/js/database/course.js
+++ src/js/database/course.js
@@ -1,4 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
 import {
     getFirestore,
     collection,
@@ -12,19 +11,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-firestore.js";
 
 const nameDbCourse = "course";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyDl8Hfe5sROEFMZdZIN9epN7UN0krD5DWk",
-    authDomain: "education-ccsxhack.firebaseapp.com",
-    projectId: "education-ccsxhack",
-    storageBucket: "education-ccsxhack.appspot.com",
-    messagingSenderId: "75636545417",
-    appId: "1:75636545417:web:947761e2502a07020a3358"
-};
-
-const app = initializeApp(firebaseConfig);
-
-export const db = getFirestore();
+const db = getFirestore();
 
 export const createCourse = (title, description) =>
     addDoc(collection(db, nameDbCourse), { title, description });
